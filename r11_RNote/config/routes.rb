16736f4e9639498856e67222c20d9dccf6367e25
MAshraf <ee.mashraf@gmail.com>
2017-05-05 07:50:31 +0300

    custom root

--- r11_RNote/config/routes.rb
+++ r11_RNote/config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   get 'welcome/index'
   resources :notes
   
+  authenticated :user do
+    root "notes#index", as: "authenticated_root"
+  end
+  
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
