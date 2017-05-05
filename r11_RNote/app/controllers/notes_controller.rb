c60a4e0c3c5011f0ce96b8666d700a24efa03699
MAshraf <ee.mashraf@gmail.com>
2017-05-05 07:01:01 +0300

    structure fin.

--- r11_RNote/app/controllers/notes_controller.rb
+++ r11_RNote/app/controllers/notes_controller.rb
@@ -1,18 +1,22 @@
 class NotesController < ApplicationController
   before_action :find_note, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
   def index
-    @notes = Note.all.order("created_at DESC")
+    @notes = Note.where(user_id: current_user)
+  end
+  
+  def new
+    @note = current_user.notes.build
   end
   
   def show
-    @note = Note.new
   end
   
   def edit
   end
   
   def create
-    @note = Note.new(note_params)
+    @note = current_user.notes.build(note_params)
     if @note.save
       redirect_to @note
     else
