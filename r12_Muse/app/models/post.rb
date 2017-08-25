5a40823dd95255b7b9b4861b801743a098e3fa44
MAshraf <ee.mashraf@gmail.com>
2017-08-25 09:11:55 +0300

    paperclip config

--- r12_Muse/app/models/post.rb
+++ r12_Muse/app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_attached_file :image, styles: { medium: "600x400>" }validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
