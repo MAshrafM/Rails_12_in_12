f2bd2818a161e36c4618c0235b394a6ce9e9427c
MAshraf <ee.mashraf@gmail.com>
2017-08-28 18:17:08 +0300

    fix issues

--- r12_Muse/app/controllers/application_controller.rb
+++ r12_Muse/app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   before_filter :configure_permitted_parameters, if: :devise_controller?
   
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :name
-    devise_parameter_sanitizer.for(:account_update) << :name
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 end
